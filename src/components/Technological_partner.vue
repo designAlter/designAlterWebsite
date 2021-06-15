b10832e5a65382e0404aac34797eec79c187ca9f
Juan Jose Castro Cruz <jcastro@cafetosoftware.com>
2021-06-15 14:16:23 -0500

    DXP-116:Setup the contact form according to the workflow explained by the customer
    Add functionally to send emails

--- src/components/Technological_partner.vue
+++ src/components/Technological_partner.vue
@@ -9,10 +9,10 @@
           </h2>
           <a href="https://cafeto.co/" target="_blank">
             <g-image
-                id="poweredBy"
-                center
-                alt="Example image"
-                src="../assets/images/PoweredBy.png"
+              id="poweredBy"
+              center
+              alt="Example image"
+              src="../assets/images/PoweredBy.png"
             />
           </a>
         </b-col>
@@ -32,26 +32,40 @@
         </b-col>
 
         <b-col class="form_section" md="6">
-          <b-form inline>
+          <b-form @submit="OnSubmit" @reset="onReset" inline>
             <b-row class="mt-2">
               <b-col md="6">
                 <label class="sr-only mb-1" for="Name">Nombre Completo</label>
-                <b-form-input id="Name" class="mb-2 mr-sm-2 mb-sm-0">
+                <b-form-input
+                  id="Name"
+                  class="mb-2 mr-sm-2 mb-sm-0"
+                  v-model="form.name"
+                  required
+                >
                 </b-form-input>
               </b-col>
               <b-col md="6">
                 <label class="sr-only mb-1" for="Compañia">Compañia</label>
-                <b-form-input id="Compañia" class="mb-2 mr-sm-2 mb-sm-0">
+                <b-form-input
+                  id="Compañia"
+                  class="mb-2 mr-sm-2 mb-sm-0"
+                  v-model="form.company"
+                  required
+                >
                 </b-form-input>
               </b-col>
             </b-row>
             <b-row class="mt-2">
               <b-col md="6">
-                <label class="sr-only mb-1" for="Email">Correo Electrónico</label>
+                <label class="sr-only mb-1" for="Email">
+                  Correo Electrónico
+                </label>
                 <b-form-input
-                  v-model="email"
+                  v-model="form.email"
                   id="Email"
                   class="mb-2 mr-sm-2 mb-sm-0"
+                  type="email"
+                  required
                 >
                 </b-form-input>
               </b-col>
@@ -59,8 +73,10 @@
                 <label class="sr-only mb-1" for="Asunto">Asunto</label>
                 <b-form-input
                   id="Asunto"
+                  v-model="form.subject"
                   class="mb-2 mr-sm-2 mb-sm-0"
                   placeholder=""
+                  required
                 >
                 </b-form-input>
               </b-col>
@@ -73,12 +89,16 @@
                   size="lg"
                   class="mb-2 mr-sm-2 mb-sm-3"
                   rows="6"
+                  v-model="form.message"
+                  required
                 ></b-form-textarea>
               </b-col>
             </b-row>
             <b-row align-h="end">
               <b-col md="5">
-                <b-button class="w-100" type="submit" variant="dark">Enviar</b-button>
+                <b-button class="w-100" type="submit" variant="dark">
+                  Enviar
+                </b-button>
               </b-col>
             </b-row>
           </b-form>
@@ -89,40 +109,78 @@
 </template>
 
 <script>
+import { VueReCaptcha, useReCaptcha } from "vue-recaptcha-v3";
+
 export default {
   data() {
     return {
       form: {
-        email: "",
-        name: "",
-        food: null,
-        checked: [],
+        name: "JuanjoName",
+        company: "TestCafeto",
+        email: "jc@gm.com",
+        subject: "contacto Tecnico",
+        message: "Hola Design Alter esto es un test",
       },
-      foods: [
-        { text: "Select One", value: null },
-        "Carrots",
-        "Beans",
-        "Tomatoes",
-        "Corn",
-      ],
-      show: true,
     };
   },
   methods: {
-    onSubmit(event) {
+    async postData(url = "https://api.sendinblue.com/v3/smtp/email") {
+      var myHeaders = new Headers();
+      myHeaders.append(
+        "api-key",
+        "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"
+      );
+      myHeaders.append("Content-Type", "application/json");
+      myHeaders.append(
+        "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",
+        "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"
+      );
+      var bodyEmail = JSON.stringify({
+        name: this.form.name,
+        subject: "[Contacto Pagina web - Design alter]",
+        sender: {
+          name: "DesignAlterWeb",
+          email: "juanjosecastrocruz@gmail.com",
+        },
+        to: [
+          {
+            email: "jcastro@cafetosoftware.com",
+            name: "Juanjo",
+          },
+        ],
+        templateId: 2,
+        params: {
+          NOMBRE: this.form.name,
+          COMPANY: this.form.company,
+          SUBJECT: this.form.subject,
+          SMS: this.form.email,
+          MESSAGE: this.form.message,
+        },
+      });
+      const response = await fetch(url, {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        headers: myHeaders,
+        body: bodyEmail,
+      });
+      return response.json();
+    },
+
+    OnSubmit(event) {
       event.preventDefault();
       alert(JSON.stringify(this.form));
+      this.postData().then((data) => {
+        console.log(data);
+      });
     },
     onReset(event) {
       event.preventDefault();
-      this.form.email = "";
-      this.form.name = "";
-      this.form.food = null;
-      this.form.checked = [];
-      this.show = false;
-      this.$nextTick(() => {
-        this.show = true;
-      });
+      (this.form.name = ""),
+        (this.form.company = ""),
+        (this.form.email = ""),
+        (this.form.subject = ""),
+        (this.form.message = "");
     },
   },
 };
